8896f6cd17cda8f2852f89885d3fdaebc2298946
plutaozin <fabiomarques2603@gmail.com>
2026-06-04 17:57:54 -0300

    projetil
    tem erro no codigo do projetil nao sei por que aaaaaa

--- scripts/bone.gd
+++ scripts/bone.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
+@onready var ani: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed = 1
-var direction = 1
+var speed = 100
+var dir = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += speed * delta
+	position.x += speed * delta * dir
+
+func set_direction(direction):
+	dir = direction
+	ani.flip_h = dir < 0
--- scripts/skeletron.gd
+++ scripts/skeletron.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 enum skeletronState {
 	walk,
 	dead,
-	spawn
+	spawn,
+	attack
 }
 @onready var ani: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $hitbox
 @onready var walldetector: RayCast2D = $walldetector
 @onready var grounddetctor: RayCast2D = $grounddetctor
+@onready var player_detector: RayCast2D = $playerDetector
+const BONE = preload("uid://ck8vii4fsih1k")
 
 var direction = 1
 
@@ -16,7 +19,7 @@ const SPEED = 30.0
 const JUMP_VELOCITY = -400.0
 
 var status : skeletronState
-
+var can_shot = true
 func _ready() -> void:
 	go_to_walk_state()
 	return
@@ -30,6 +33,8 @@ func _physics_process(delta: float) -> void:
 			walk_state(delta)
 		skeletronState.dead:
 			dead_state(delta)
+		skeletronState.attack:
+			attack_state(delta)
 		
 	move_and_slide()
 
@@ -42,7 +47,11 @@ func go_to_dead_state():
 	hitbox.process_mode = Node.PROCESS_MODE_DISABLED
 	velocity = Vector2.ZERO
 	
-
+func go_to_attack_state():
+	status = skeletronState.attack
+	ani.play("attack")
+	velocity = Vector2.ZERO
+	can_shot = true
 func walk_state(_delta):
 	velocity.x = SPEED * direction
 	
@@ -52,24 +61,24 @@ func walk_state(_delta):
 	if !grounddetctor.is_colliding():
 		scale.x *= -1
 		direction *= -1
+	if player_detector.is_colliding():
+		go_to_attack_state()
+		return
 func dead_state(_delta):
 	pass
-	
+func attack_state(_delta):
+	if ani.frame == 2 && can_shot:
+		shot_bone()
+		can_shot = false
 func take_damage():
 	go_to_dead_state()
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+func shot_bone():
+	var new_bone = BONE.instantiate()
+	add_sibling(new_bone)
+	new_bone.position = self.position
+	new_bone.set_direction(direction)
+		
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if ani.animation == "attack":
+		go_to_walk_state()
+		return
