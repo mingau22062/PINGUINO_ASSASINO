6f5089a825cb02ca827d609a4b0225aed9da37c9
plutaozin <fabiomarques2603@gmail.com>
2026-06-06 20:15:45 -0300

    commit projetil 1.1
    configurando projetil ainda

--- scripts/bone.gd
+++ scripts/bone.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var ani: AnimatedSprite2D = $AnimatedSprite2D
+@onready var detrution_timer: Timer = $detrutionTimer
 
 var speed = 100
 var dir = 1
@@ -13,3 +14,11 @@ func _process(delta: float) -> void:
 func set_direction(direction):
 	dir = direction
 	ani.flip_h = dir < 0
+
+
+func _on_detrution_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
